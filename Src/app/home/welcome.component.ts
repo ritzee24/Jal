05e50e4dca7630992d51b1845d6c2c8f86bcdc0f
ritzee24 <ritikapandey34@gmail.com>
2020-03-13 10:16:07 +0530

    welcome  page

---
+++ Src/app/home/welcome.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './welcome.component.html'
+})
+export class WelcomeComponent {
+  public pageTitle = 'Welcome ! Thousands have lived without love , not one without water.';
+}
